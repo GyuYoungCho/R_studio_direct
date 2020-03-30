1c1e26574297289bd646d977c59d2245f1950b7d
GyuYoungCho <rbud613@gmail.com>
2020-03-30 14:37:19 +0900

    commit

--- PracticalMachineLearning/practicalmachinelearning4.R
+++ PracticalMachineLearning/practicalmachinelearning4.R
@@ -26,7 +26,6 @@ sqrt(sum((pred2-testing$wage)^2))
 sqrt(sum((combPred-testing$wage)^2))
 
 
-
 install.packages("quantmod")
 library(quantmod)
 install.packages("forecast")
@@ -52,3 +51,58 @@ ets1 <- ets(ts1Train,model="MMM")
 fcast <- forecast(ets1)
 plot(fcast); lines(ts1Test,col="red")
 accuracy(fcast,ts1Test)
+
+
+
+#quiz
+library(gbm)
+library(AppliedPredictiveModeling)
+set.seed(3433)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+
+set.seed(62433)
+mod1 = train(diagnosis ~.,method="rf",data=training,verbose=FALSE)
+mod2 = train(diagnosis ~.,method="gbm",data=training,verbose=FALSE)
+mod3 = train(diagnosis ~.,method="lda",data=training,verbose=FALSE)
+pred1 = predict(mod1,testing) 
+pred2 = predict(mod2,testing) 
+pred3 = predict(mod3,testing) 
+confusionMatrix(pred1,testing$diagnosis)
+confusionMatrix(pred2,testing$diagnosis)
+confusionMatrix(pred3,testing$diagnosis)
+
+set.seed(3523)
+data(concrete)
+inTrain = createDataPartition(concrete$CompressiveStrength, p = 3/4)[[1]]
+training = concrete[ inTrain,]
+testing = concrete[-inTrain,]
+set.seed(233)
+mod1 = train(CompressiveStrength ~.,method="lasso",data=training)
+
+set.seed(325)
+library(e1071)
+mod1 = svm(CompressiveStrength ~.,data=training)
+pred = predict(mod1,testing)
+RMSE = function(m, o){
+    sqrt(mean((m - o)^2))
+}
+RMSE(pred,testing$CompressiveStrength)
+
+install.packages("lubridate")
+library(lubridate) # For year() function below
+
+dat = read.csv("gaData.csv")
+
+training = dat[year(dat$date) < 2012,]
+
+testing = dat[(year(dat$date)) > 2011,]
+
+tstrain = ts(training$visitsTumblr)
+tstest = ts(testing$visitsTumblr)
+ets1 <- ets(tstrain)
+fcast <- forecast(ets1)
+accuracy(fcast,tstest)
